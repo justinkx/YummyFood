fa34677f04b4fa506409162db4aa735af1cb9e4f
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2019-06-03 22:01:45 +0530

    added offers screen

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  slideOpts:any = {
+    initialSlide: 0,
+    speed: 400,
+    spaceBetween: 10,
+    pagination: true,
+    slidesPerView: 2
+  };
+  categories = <any>[];
+  recomented = <any>[];
+  popular = <any>[];
+  searchWord:string;
+  constructor(private platform: Platform,
+    private http: HttpClient) {
+      console.log(this.platform);
+      this.slideOpts.width = this.platform.is('mobile') ? this.platform.width() - 32 :
+      this.platform.width()/2 - 32;
+    this.slideOpts.height = this.platform.height()/3;
+  }
 
-  constructor() {}
-
+  ngOnInit() {
+    forkJoin({
+      category: this.http.get('./assets/data/food.json'),
+      events: this.http.get('./assets/data/categories.json')
+    })
+    .subscribe((response:any) => {
+      console.log(response);
+      this.categories = response.category.category;
+      this.recomented = response.events.categories[0].items;
+      this.popular = response.events.categories[1].items;
+    });
+  }
 }
